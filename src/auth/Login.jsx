6af491e0f1d2ddb18bf87b2a45977a9097377e31
henrycardenas89 <henrycarden@gmail.com>
2022-11-30 13:50:12 -0600

    added Spotify API routes to Login file

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -31,7 +31,9 @@ export default function Login() {
 
   const [token, setToken] = useState("");
   const [myInfo, setMyInfo] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
 
+  // Set Token and store in Local Storage
   useEffect(() => {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
@@ -49,18 +51,123 @@ export default function Login() {
     setToken(token);
   }, []);
 
+  // Remove Token from Local Storage
   const logout = () => {
     setToken("");
     window.localStorage.removeItem("token");
   };
 
-  const me = async () => {
+  // // GET Artist
+  // const getArtists = async (artistId) => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       `https://api.spotify.com/v1/artists/${artistId}`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // GET Several Artists
+  // const getSeveralArtists = async (artist1, artist2, artist3) => {
+  //   try {
+  //     const { data } = await axios.get("https://api.spotify.com/v1/artists", {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //       params: {
+  //         ids: `${artist1},${artist2},${artist3}`, // separate artist ids by comma
+  //       },
+  //     });
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // GET Artist's Albums
+  // const getArtistAlbums = async (artistId) => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       `https://api.spotify.com/v1/artists/${artistId}/albums`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // Search Artists
+  // const searchArtists = async (e) => {
+  //   e.preventDefault();
+  //   const { data } = await axios.get("https://api.spotify.com/v1/search", {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //     params: {
+  //       q: searchKey,
+  //       type: "artist",
+  //     },
+  //   });
+  //   console.log(data);
+  // };
+
+  // // GET User's Saved Albums
+  // const getUserSavedAlbums = async () => {
+  //   try {
+  //     const { data } = await axios.get("https://api.spotify.com/v1/me/albums", {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // GET Current User's Profile
+  // const getCurrentUserProfile = async () => {
+  //   try {
+  //     const { data } = await axios.get("https://api.spotify.com/v1/me", {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // GET Current User's Top Items (Artists or Tracks)
+  const getCurrentUserTopItems = async () => {
     try {
       const { data } = await axios.get(
-        "https://api.spotify.com/v1/me/playlists",
+        "https://api.spotify.com/v1/me/top/tracks",
         {
           headers: {
             Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          params: {
+            time_range: "short_term",
           },
         }
       );
@@ -70,6 +177,45 @@ export default function Login() {
       console.log(e);
     }
   };
+
+  // // GET Genres
+  // const getGenres = async () => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       "https://api.spotify.com/v1/browse/categories",
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //           "Content-Type": "application/json",
+  //         },
+  //         params: {
+  //           country: "US",
+  //         },
+  //       }
+  //     );
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // GET Playlist
+  // const getPlaylist = async (playlistId) => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       `https://api.spotify.com/v1/playlists/${playlistId}`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     console.log(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   return (
     <div>
@@ -84,7 +230,6 @@ export default function Login() {
           <button
             className="bg-teal-500 hover:bg-teal-700 h-10 w-40 active:ring"
             type="submit"
-            href={AUTH_URL}
           >
             Login
           </button>
@@ -97,10 +242,17 @@ export default function Login() {
           <a className="flex justify-center items-center">
             <button
               className="bg-teal-500 hover:bg-teal-700 h-10 w-40 active:ring"
-              onClick={me}
+              onClick={getCurrentUserTopItems}
             >
               Get My Info
             </button>
+            {/* <form onSubmit={searchArtists}>
+              <input
+                type="text"
+                onChange={(e) => setSearchKey(e.target.value)}
+              />
+              <button type={"submit"}>Search</button>
+            </form> */}
           </a>
           <a className="flex justify-center items-center h-screen p-20">
             <button
