1e2ebde90819694611212f608440c6543d465ce4
LLU27 <107891664+LLU27@users.noreply.github.com>
2022-12-02 11:18:19 -0500

    finished login page

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -5,6 +5,8 @@ import { connect, useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchUserArtists, fetchUserTracks } from "../redux/userTopItems";
 import { fetchCurrentUserProfile } from "../redux/userProfile";
+import LoginPage from "../components/LoginPage";
+import { data } from "autoprefixer";
 
 export default function Login() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
@@ -68,17 +70,13 @@ export default function Login() {
     window.localStorage.removeItem("token");
   };
 
-
   const me = async () => {
     try {
-      const { data } = await axios.get(
-        "https://api.spotify.com/v1/me/",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const { data } = await axios.get("https://api.spotify.com/v1/me/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       setMyInfo(data.items);
       console.log(data);
     } catch (e) {
@@ -209,158 +207,29 @@ export default function Login() {
   //   }
   // };
 
-
-  //TESTED DB FETCH ID
-  //done with vibe
-    //adding a vibe, mutual still false
-    const vibe = async ()=>{
-      try {
-        const { data, error } = await supabase
-      .from('Vibe')
-      .insert([
-      { friendId: 6, userId: 10 },
-    ])
-    console.log('clicked')
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    //delete a vibe && setMutual false
-    const deleteVibe = async (userId, friendId)=>{
-      try {
-        const { data, error } = await supabase
-      .from('Vibe')
-      .delete()
-      .match({userId:userId,friendId:friendId})
-      let {data:deleteMutual}  = await supabase.from('Vibe').update({ mutual: 'false' }).match({ userId:friendId, friendId:userId })
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-  
-  // fetching user's friends
-  const superbaseDB = async () => {
-    try {
-      let { data: friends, error } = await supabase.from("Vibe").select("friendId").eq("userId",6);
-      console.log("before map",friends);
-
-     let users = await Promise.all(friends.map(friend => 
-       supabase
-      .from("User")
-      .select("*")
-      .eq("id",friend.friendId)))
-      console.log("after map",users[0].data)
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  //add categories upon 1st time User's pick
-  const addCategories =async ()=>{
-    try {
-      let { data: users, error } = await supabase
-  .from('User_Top_Cat')
-  .select('*') 
-  console.log(users)
-  let filteredUsers = users.filter(user=>user.favCats.includes('pop'))
-  console.log(filteredUsers)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  //checkMutual 
-  //ex. check before writing on wall
-  const checkMutual = async() => {
-    try {
-      //find status of one user's mutual, if one is true assume the other is also true. in this case checking userid 6
-      let {data : user}  = await supabase.from("Vibe").select("mutual").match({userId:6,friendId:10})
-      console.log(user)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
-
-  //change status to true on both user rows
-  const setMutual = async()=>{
-      //update mutual status of one user, if one is false, will need to update the other one
-      try{
-      let {data:setMutualA}  = await supabase.from('Vibe').update({ mutual: 'true' }).match({userId:6,friendId:10})
-      console.log(setMutualA)
-      let {data:setMutualB}  = await supabase.from('Vibe').update({ mutual: 'true' }).match({userId:10,friendId:6})
-      console.log(setMutualB)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
-//Jerry goes to Le's page they are strangers, useEffect runs SELECT on Vibe table JerryUser and LeFriend check if row exists, 
-//if return null, you did not vibe yet, so vibe button is "VIBE".
-//Jerry clicks on vibe,  it will run the vibe function to add the row with jerry USER le Friend.
-//check relationship exist in the opposite, where Le user Jerry friend. if returns a row, then run setMutual on both side.
-//with Le's Userid check Jerry Friendid,if returns null = then dont do anything.
-
-
-//if returns a row,button will show "VIBED" check mutual value.
-    //if mutual=true, allow write on wall. else cannot write on wall
-
-//two days, Le goes on Jerrys page after jerry already vibed, useEffects run checkmutual(), which still return false at this point. Le clicks on vibe, whichs runs the vibe function to add row Le User Jerry Friend. where Jerry user Le friend. if both rows exist, run setMutual.
-
-
-
-//WALL POSTS
-//(posterId)userID_ONE writes on (userId)userID_TWO's wall.
-const postOnWall = async ()=>{
-  try {
-    const { data:wallpost, error } = await supabase
-    .from('Wall_Post')
-    .insert([
-      { userId: 1, posterId:2},
-    ])
-console.log('wallpost',wallpost)
-console.log('clicked')
-  }catch(error){
-    console.log(error)
-  }
-}
-
-
+ 
   return (
     <div>
-      <h1 className="flex justify-center items-center font-medium text-6xl">
-        Vibe
-      </h1>
       {!token ? (
-        <a
-          className="flex justify-center items-center h-screen p-20"
-          href={AUTH_URL}
-        >
-          <button
-            className="bg-teal-500 hover:bg-teal-700 h-10 w-40 active:ring"
-            type="submit"
+        <div>
+          <LoginPage />
+          <a
+            className="flex justify-center items-center h-screen p-20"
+            href={AUTH_URL}
           >
-            Login
-          </button>
-        </a>
+            <button
+              className="bg-teal-500 hover:bg-teal-700 h-10 w-40 active:ring"
+              type="submit"
+            >
+              Login
+            </button>
+          </a>
+        </div>
       ) : (
         <div>
           <h1 className="flex justify-center items-center font-medium text-3xl">
             Welcome
           </h1>
-
-          <button onClick={superbaseDB}>getfriend</button>
-          <button onClick={vibe}>Vibe</button>
-          <button onClick={addCategories}>addCategories</button>
-          <button onClick={checkMutual}>checkMutual</button>
-          <button onClick={setMutual}>setMutual</button>
-          <button onClick={()=>deleteVibe(6,10)}>deleteVibe</button>
-          <button onClick={postOnWall}>postOnWall</button>
-
           {/* <a className="flex justify-center items-center">
 
             <button
@@ -377,6 +246,7 @@ console.log('clicked')
               <button type={"submit"}>Search</button>
             </form>
           </a> */}
+
           <a className="flex justify-center items-center h-screen p-20">
             <button
               className="bg-teal-500 hover:bg-teal-700 h-10 w-40 active:ring"
@@ -386,9 +256,7 @@ console.log('clicked')
             </button>
           </a>
         </div>
-        )
-      }
+      )}
     </div>
-);
-        }
-        
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        inter: ["inter", "serif"],
+      backgroundSize: {
+        "size-200": "200% 200%",
+      },
+      backgroundPosition: {
+        "pos-0": "0% 0%",
+        "pos-100": "100% 100%",
       },
     },
   },
   plugins: [],
-};
+}
