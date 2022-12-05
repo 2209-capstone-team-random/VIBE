ac9889f3901fb2b66edcdedeffc70aa268aba33f
Jerry Wu <jerrywu.hr@gmail.com>
2022-12-05 10:37:24 -0800

    t ignore temp commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import store from "./store";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from './store';
+import App from './App';
+import DrawerHolder from './DrawerHolder';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <App/>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route path="/dh" element={<DrawerHolder />} />
+        </Routes>
       </BrowserRouter>
     </Provider>
-  /* </React.StrictMode> */
-
-); 
+  </React.StrictMode>
+);
