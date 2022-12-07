51c235849d0fff3fba2aec75e890d232e30b18f9
LLU27 <107891664+LLU27@users.noreply.github.com>
2022-12-07 13:08:47 -0500

    1

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,19 +1,27 @@
-
-import React, { useState } from 'react';
-import { getCount } from '../../redux/dbQueryThunks/user';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { supabase } from '../../supabaseClient';
+import React, { useState } from "react";
+import { getCount } from "../../redux/dbQueryThunks/user";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { supabase } from "../../supabaseClient";
 
 const Card = () => {
-  const [catePicked, setCatePicked] = useState(0);
+  const [categoryPicked, setCategoryPicked] = useState(0);
+  const [genres, setGenres] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [genre, setGenre] = useState([]);
   const dispatch = useDispatch();
-
+  const colors = [
+    "bg-blue-500",
+    "bg-pink-400",
+    "bg-green-500",
+    "bg-red-400",
+    "bg-gray-500",
+    "bg-orange-500",
+    "bg-red-500",
+    "bg-blue-700",
+  ];
   const getCards = async () => {
     try {
-      let { data } = await supabase.from('Categories').select('*');
+      let { data } = await supabase.from("Categories").select("*");
       setCategories(data);
     } catch (error) {
       console.error(error);
@@ -21,13 +29,11 @@ const Card = () => {
   };
 
   useEffect(() => {
-    dispatch(getCount(catePicked));
-    console.log(catePicked);
+    dispatch(getCount(categoryPicked));
     if (categories.length < 1) {
       getCards();
     }
-  }, [catePicked, categories]);
-
+  }, [categoryPicked]);
 
   const pickThreeCate = (event) => {
     if (categoryPicked < 3) {
@@ -52,18 +58,16 @@ const Card = () => {
   return (
     <>
       <div className=" flex items-center justify-center absolute space-x-5 flex-wrap ">
-        {categories.map((el) => (
-
+        {categories.map((category, idx) => (
           <div
             key={category.id}
             className={
               genres.includes(category.name)
-                ? `scale-125 ${category.color} p-5 rounded-lg w-40 group `
-                : `transition ease-in-out dcategoryay-0 ${category.color} hover:scale-105 duration-300 p-5 rounded-lg w-40 group `
+                ? `scale-125 ${colors[idx]} p-5 rounded-lg w-40 group `
+                : `transition ease-in-out dcategoryay-0 ${colors[idx]} hover:scale-105 duration-300 p-5 rounded-lg w-40 group `
             }
             onClick={pickThreeCate}
           >
-
             <img src={category.img} className="w-full rounded shadow" />
             <h3 id="name" className="text-gray-200 font-bold mt-5">
               {category.name}
