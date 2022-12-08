c0f8f8d233378be0135dd0b143e10f5f8a61bc44
henrycardenas89 <henrycarden@gmail.com>
2022-12-08 17:02:46 -0600

    top artists

--- src/components/Home/CurrentUserProfile.jsx
+++ src/components/Home/CurrentUserProfile.jsx
@@ -3,22 +3,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import TopPlaylists from "./TopPlaylists";
 import SpotifyPlayer from "react-spotify-web-playback";
-import { fetchUserTracks } from "../../redux/Spotify/userTopTracks";
 import { fetchUserByIdPlaylists } from "../../redux/Spotify/userPlaylists";
 import NameBio from "./NameBio";
 import WallPosts from "./WallPosts";
 import NavBar from "./Navbar";
 import { useNavigate, useParams } from "react-router-dom";
 import TopTracks from "./TopTracks";
+import TopArtists from "./TopArtists";
 
 export default function CurrentUserProfile({ token, session }) {
   const dispatch = useDispatch();
-  // const { items } = useSelector((store) => store.userTopTracks);
   const { items } = useSelector((store) => store.userPlaylists);
   const { userId } = useParams();
 
   useEffect(() => {
-    dispatch(fetchUserTracks(token));
     dispatch(fetchUserByIdPlaylists(userId, token));
   }, [token]);
   if (items) {
@@ -28,6 +26,7 @@ export default function CurrentUserProfile({ token, session }) {
         <NameBio session={session} userId={userId} />
         <TopPlaylists session={session} token={token} />
         <TopTracks session={session} token={token} />
+        <TopArtists session={session} token={token} />
         <WallPosts session={session} />
         <div className="fixed z-10 bottom-0 mt-10 w-full">
           <SpotifyPlayer token={token} uris={items.map((item) => item.uri)} />
