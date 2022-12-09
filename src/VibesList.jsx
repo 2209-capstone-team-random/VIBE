3e085cf68f763935d9250d5739c4bfcaccd72c0f
henrycardenas89 <henrycarden@gmail.com>
2022-12-09 17:03:15 -0600

    minor edits to css and conditional

--- src/VibesList.jsx
+++ src/VibesList.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { supabase } from './supabaseClient';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient";
+import { Link, useNavigate } from "react-router-dom";
 
 const VibesList = ({ vibes, ownId }) => {
   const navigate = useNavigate();
   return (
     <>
-      <h2 className="grid ml-2">VIBEES!</h2>
-      <ul className="menu bg-base-100 w-56 dark:bg-zinc-600 ">
-        <li key={'homelink'}>
-          <a href={`../profile/${ownId}`}>Home</a>
-        </li>
+      <h2 className="grid ml-4">VIBEES!</h2>
+      <ul className="menu bg-base-100 w-56 dark:bg-zinc-600">
         {vibes.map((vibe) => {
           return (
             <li key={vibe.id}>
@@ -19,6 +16,12 @@ const VibesList = ({ vibes, ownId }) => {
           );
         })}
       </ul>
+
+      <div key={"homelink"}>
+        <a href={`../profile/${ownId}`} className="ml-4">
+          Home
+        </a>
+      </div>
     </>
   );
 };
